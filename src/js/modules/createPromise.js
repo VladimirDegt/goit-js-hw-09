2ce7740313dd8e7d0da25327e40762438dbf6966
Vladimir <107486166+VladimirDegt@users.noreply.github.com>
2023-04-20 12:37:22 +0300

    update 03

--- src/js/modules/createPromise.js
+++ src/js/modules/createPromise.js
@@ -1,12 +1,14 @@
 export function createPromise(position, delay) {
   return new Promise( (resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
-    setTimeout( () => {
         if (shouldResolve) {
-          resolve({position, delay})
+          setTimeout( () => {
+            resolve({position, delay})
+          }, delay);
         } else {
+          setTimeout( () => {
           reject({position, delay})
+        }, delay);
         }
-    }, delay);
   });
 };
